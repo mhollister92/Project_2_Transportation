0a9db87f7ff3d8ef40a97e930e56985104a56bf3
mhollister92 <mhollister92@gmail.com>
2015-11-02 18:21:54 -0500

    Part 3

--- Project 2 Transportation/Assets/Airplane.cs
+++ Project 2 Transportation/Assets/Airplane.cs
@@ -5,6 +5,22 @@
 	public int x;
 	public int y;
 
-	public int cargo;
-	public int cargoCapacity;
+	private int xDirection;
+	private int yDirection;
+
+	public int cargo = 0;
+	public int cargoCapacity = 90;
+
+	public void SetMoveDirection(int xMoveDirection, int yMoveDirection)
+	{
+		xDirection = xMoveDirection;
+		yDirection = yMoveDirection;
+	}
+
+	public void MoveAirplane()
+	{
+			x += xDirection;
+			y += yDirection;
+		    SetMoveDirection (0, 0);
+	}
 }
--- Project 2 Transportation/Assets/GameController.cs
+++ Project 2 Transportation/Assets/GameController.cs
@@ -9,6 +9,10 @@ public class GameController : MonoBehaviour {
 	private GameObject [,] allCubes;	//array to keep track of cubes
 	Airplane myAirplane;
 
+	float turn = 1.5f;
+	int score = 0;
+	float timeToAct;
+
 	//Function to change color of clicked cube
 	public void ProcessClickedCube (GameObject clickedCube, int x, int y)
 	{
@@ -22,13 +26,14 @@ public class GameController : MonoBehaviour {
 			myAirplane.active = false;
 			clickedCube.GetComponent<Renderer> ().material.color = Color.red;
 		} 
-		//move airplane location and turn old airplane white
+		/*move airplane location and turn old airplane white
 		else if ((x != myAirplane.x || y != myAirplane.y) && myAirplane.active) {
 			allCubes[myAirplane.x, myAirplane.y].GetComponent<Renderer>().material.color = Color.white;
 			allCubes[x,y].GetComponent<Renderer>().material.color = Color.yellow;
 			myAirplane.x = x;
 			myAirplane.y = y;
 		}
+		*/
 	}
 
 	// Use this for initialization
@@ -51,10 +56,75 @@ public class GameController : MonoBehaviour {
 		myAirplane.x = 0;
 		myAirplane.y = 8;
 		allCubes [0, 8].GetComponent<Renderer> ().material.color = Color.red;
+
+		//make bottom corner depot and turn black (Need to keep it from turning white after airplane is there)
+		allCubes [15, 0].GetComponent<Renderer> ().material.color = Color.black;
+
+		timeToAct = turn;
 	}
-	
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (5, 1, 100, 20), "Score: " + score);
+		GUI.Label (new Rect (150, 1, 100, 20), "Cargo: " + myAirplane.cargo);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+		//record the move direction of the airplane and make sure it is on the grid
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			if (myAirplane.y < 8)
+			{
+				myAirplane.SetMoveDirection (0, 1);
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (myAirplane.y > 0)
+			{
+				myAirplane.SetMoveDirection(0, -1);
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if(myAirplane.x > 0)
+			{
+				myAirplane.SetMoveDirection(-1, 0);
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (myAirplane.x < 15)
+			{
+				myAirplane.SetMoveDirection(1, 0);
+			}
+		}
+		//keeps depot black
+		if (myAirplane.x != 15 || myAirplane.y != 0) {
+			allCubes [15, 0].GetComponent<Renderer> ().material.color = Color.black;
+		}
+		//things to do every turn
+		if (Time.time >= timeToAct && myAirplane.active) {
+			//turn old airplane spot into sky
+			allCubes[myAirplane.x, myAirplane.y].GetComponent<Renderer>().material.color = Color.white;
+			//move airplane
+			myAirplane.MoveAirplane();
+			//turn new airplane cube yellow
+			allCubes[myAirplane.x, myAirplane.y].GetComponent<Renderer>().material.color = Color.yellow;
+			//if cube is on start spot, increase cargo
+			if((myAirplane.x == 0 && myAirplane.y == 8) && myAirplane.active)
+			{
+				if(myAirplane.cargo < myAirplane.cargoCapacity)
+				{
+					myAirplane.cargo += 10;
+				}
+			}
+			//if cube is on depot, empty cargo
+			else if((myAirplane.x == 15 && myAirplane.y == 0) && myAirplane.active)
+			{
+				score += (myAirplane.cargo / 10);
+				myAirplane.cargo = 0;
+			}
+
+			timeToAct += turn;
+		}
+}
 }
